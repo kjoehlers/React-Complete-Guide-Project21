1df37f4c1b56a03cfff3c2ca62f8943ea2fef4f6
Ken Oehlers <kjojr56@gmail.com>
2023-03-30 14:11:35 -0400

    Commit before Lesson 287

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,6 +5,7 @@ import EventsList from "../components/EventsList";
 function EventsPage() {
   const events = useLoaderData();
 
+  // return <EventsList events={events} />;
   return <EventsList events={events} />;
 }
 
