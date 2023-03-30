947eab914181f92aff900dea4a1f91b731b93fda
Ken Oehlers <kjojr56@gmail.com>
2023-03-30 13:43:36 -0400

    Commit before Lesson 286

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,24 +1,11 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-const EVENTS = [
-  { id: "e1", title: "Event 1" },
-  { id: "e2", title: "Event 2" },
-  { id: "e3", title: "Event 3" },
-];
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  return (
-    <>
-      <h1>Events Page</h1>
-      <ul>
-        {EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  const events = useLoaderData();
+
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
