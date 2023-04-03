46806dcddd5a77a298ce2130efd4f04d93813d0c
Ken Oehlers <kjojr56@gmail.com>
2023-04-03 17:59:00 -0400

    Commit at End of Section 20

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,10 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <PageContent title="Welcome to the Events App!">
+      <p>Browse through our events and sign up for the ones you like!</p>
+    </PageContent>
   );
 }
 
